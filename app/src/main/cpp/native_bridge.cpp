9c5edabd57f320eb05607a08385c7d58635a9616
hirad990 <iamhirad17@gmail.com>
2026-09-05 16:40:37 +0330

    feat: implement JNI native bridge

--- app/src/main/cpp/native_bridge.cpp
+++ app/src/main/cpp/native_bridge.cpp
@@ -1,6 +1,18 @@
+#include <jni.h>
 #include <android/log.h>
 
-extern "C" int binerlauncher_native_ready() {
-    __android_log_print(ANDROID_LOG_INFO, "BinerLauncher", "Native bridge loaded");
+#define LOG_TAG "BinerLauncherNative"
+#define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
+
+extern "C" JNIEXPORT jstring JNICALL
+Java_ir_binercraft_binerlauncher_nativebridge_NativeBridge_nativeGetEngineInfo(
+        JNIEnv* env, jobject) {
+    return env->NewStringUTF("BinerLauncher Native Engine / Android NDK");
+}
+
+extern "C" JNIEXPORT jint JNICALL
+Java_ir_binercraft_binerlauncher_nativebridge_NativeBridge_nativeAbiVersion(
+        JNIEnv*, jobject) {
+    LOGI("Native bridge initialized");
     return 1;
 }
