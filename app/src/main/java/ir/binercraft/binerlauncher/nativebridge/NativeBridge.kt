6e618d80b0210c7782e60a7a5b5d01b8af0c5afb
hirad990 <iamhirad17@gmail.com>
2026-09-05 17:01:57 +0330

    Connect GameActivity to native surface bridge

--- app/src/main/java/ir/binercraft/binerlauncher/nativebridge/NativeBridge.kt
+++ app/src/main/java/ir/binercraft/binerlauncher/nativebridge/NativeBridge.kt
@@ -1,5 +1,7 @@
 package ir.binercraft.binerlauncher.nativebridge
 
+import android.view.Surface
+
 object NativeBridge {
     init {
         System.loadLibrary("binerlauncher-native")
@@ -7,4 +9,11 @@ object NativeBridge {
 
     external fun nativeGetEngineInfo(): String
     external fun nativeAbiVersion(): Int
+    external fun nativeAttachSurface(surface: Surface)
+    external fun nativeDetachSurface()
+    external fun nativeDispatchTouch(action: Int, x: Float, y: Float)
+
+    fun attachSurface(surface: Surface) = nativeAttachSurface(surface)
+    fun detachSurface() = nativeDetachSurface()
+    fun dispatchTouch(action: Int, x: Float, y: Float) = nativeDispatchTouch(action, x, y)
 }
