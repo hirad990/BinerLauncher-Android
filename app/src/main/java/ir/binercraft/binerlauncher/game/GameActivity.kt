cce836d68d0db99feda32308ad4e3a9917c234a8
hirad990 <iamhirad17@gmail.com>
2026-09-11 15:51:12 +0330

    feat(game): overlay touch HUD on native game surface

--- app/src/main/java/ir/binercraft/binerlauncher/game/GameActivity.kt
+++ app/src/main/java/ir/binercraft/binerlauncher/game/GameActivity.kt
@@ -5,17 +5,32 @@ import android.os.Bundle
 import android.view.MotionEvent
 import android.view.SurfaceHolder
 import android.view.WindowManager
+import android.widget.FrameLayout
 import ir.binercraft.binerlauncher.nativebridge.NativeBridge
 
 class GameActivity : Activity(), GameSurfaceView.Listener {
+    private lateinit var root: FrameLayout
     private lateinit var gameSurface: GameSurfaceView
+    private lateinit var touchHud: TouchHudView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
-        gameSurface = GameSurfaceView(this)
-        gameSurface.listener = this
-        setContentView(gameSurface)
+        window.decorView.systemUiVisibility = (
+            android.view.View.SYSTEM_UI_FLAG_FULLSCREEN or
+                android.view.View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
+                android.view.View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
+                android.view.View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+                android.view.View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+                android.view.View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            )
+
+        root = FrameLayout(this)
+        gameSurface = GameSurfaceView(this).also { it.listener = this }
+        touchHud = TouchHudView(this)
+        root.addView(gameSurface, FrameLayout.LayoutParams(-1, -1))
+        root.addView(touchHud, FrameLayout.LayoutParams(-1, -1))
+        setContentView(root)
     }
 
     override fun onSurfaceReady(surfaceHolder: SurfaceHolder) {
